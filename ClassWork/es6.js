1810c763df7868c273dfc81ee8c7e6d38bc4deb8
Ojesev Gupta <ojesevgupta2008@gmail.com>
2026-09-15 21:32:04 +0530

    es6 work

---
+++ ClassWork/es6.js
@@ -0,0 +1,16 @@
+// Normal Function
+
+function sayHello(name) {
+    console.log(`Hello, ${name}!`);
+}
+
+sayHello("Ojesev Gupta");
+
+
+// Arrow Function
+
+const sayGoodbye = (name) => {
+    console.log(`Goodbye, ${name}!`);
+};
+
+sayGoodbye("Ojesev Gupta");
